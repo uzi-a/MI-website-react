b59be553b5f81d14d086819e75a9533f3815dc83
uzi-a <84665769+uzi-a@users.noreply.github.com>
2023-02-11 15:20:50 +0000

    Separate pages working with navbar

---
+++ src/user pages/ErrorReport.js
@@ -0,0 +1,10 @@
+import { Navbar } from "../components"
+
+
+export default function ErrorReport() {
+    return (
+        <><div className="gradient__bg">
+            <Navbar />
+        </div><h1> Error Report Form </h1></>
+    )
+}
