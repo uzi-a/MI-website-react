680db02c6e1d27493e3a6cfcf6ea8661ac2d9556
uzi-a <84665769+uzi-a@users.noreply.github.com>
2023-02-11 14:28:28 +0000

    Saving before changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,58 @@
-import React from 'react'
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import React, { useState } from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
 import logo from '../../assets/logo.svg';
 import login from '../../assets/Login button.svg';
 
+const Menu = () => (
+  <>
+  <p><a href="#home">HOME</a></p>
+  <p><a href="#games">GAMES</a></p>
+  <p><a href="#help">HELP</a></p>
+  </>
+)
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="mi__navbar">
+
+      {/* Navbar top left logo - TC/AYG? */}
       <div className="mi__navbar-links">
         <div className="mi__navbar-links_logo">
-          <img src={logo} alt="logo" />
-        </div>
-        <div className="mi__navbar-links_container">
-          <p><a href="#home">HOME</a></p>
-          <p><a href="#games">GAMES</a></p>
-          <p><a href="#help">HELP</a></p>
+          <a href="#home">
+            <img src={logo} alt="logo" />
+            </a>
         </div>
       </div>
+
+      {/* Links to HOME, GAMES, HELP pages */}
+      <div className="mi__navbar-links_container">
+        <Menu />
+      </div>
+      
+      {/* Login button */}
       <div className="mi__navbar-login">
-        <img src={login} alt="login" width="100" height="50"/>
-        </div> 
+        <a href="#login">
+          <img src={login} alt="login" width="100" height="50"/>
+          </a>
+      </div>
+
+      {/* Dropdown menu (for smaller devices) */}
+      <div className="mi__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} /> 
+        }
+        {toggleMenu && (
+          <div className="mi__navbar-menu_container scale-up-center">
+            <div className = "mi__navbar-menu_container-links">
+            <Menu />
+            </div>
+          </div>
+        )}
+      </div> 
     </div>
   )
 }
